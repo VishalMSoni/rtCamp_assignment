7cdf343e232f92caf3dfea7ccb87cbc74a0b6496
VishalMSoni <vishalsoni611@gmail.com>
2019-07-29 05:57:36 +0000

    Added conf file

--- install.sh
+++ install.sh
@@ -17,63 +17,64 @@ installNginx() {
 }
 
 confConfig() {
-  sudo rm -rf /etc/nginx/sites-available/$1
-  sudo tee /etc/nginx/sites-available/$1 <<EOF
+sudo rm -rf /etc/nginx/sites-available/$1
+sudo tee /etc/nginx/sites-available/$1 <<EOF
 server {
-    listen 80;
-    listen [::]:80;
-    root /var/www/html/$1;
-    
-    index index.php index.html index.htm index.nginx-debian.html;
-    server_name $1 www.$1;
-    location / {
-        try_files $uri ${uri}/ =404;
-        try_files $uri $uri/ /index.php$is_args$args;
-    }
-    location ~ \.php$ {
-        include snippets/fastcgi-php.conf;
-        fastcgi_pass unix:/run/php/php7.2-fpm.sock;
-    }
-  }
+listen 80;
+listen [::]:80;
+root /var/www/html/$1;
+
+index index.php index.html index.htm index.nginx-debian.html;
+server_name $1 www.$1;
+location / {
+try_files $uri ${uri}/ =404;
+try_files $uri $uri/ /index.php$is_args$args;
+}
+location ~ \.php$ {
+include snippets/fastcgi-php.conf;
+fastcgi_pass unix:/run/php/php7.2-fpm.sock;
+}
+}
 EOF
-  sudo ln -s /etc/nginx/sites-available/$1 /etc/nginx/sites-enabled/
-  sudo systemctl restart nginx
-  sudo nginx -t
-  if [ $? == 0 ]; then
-    sudo systemctl reload nginx
-  fi
+sudo ln -s /etc/nginx/sites-available/$1 /etc/nginx/sites-enabled/
+sudo systemctl restart nginx
+sudo nginx -t
+if [ $? == 0 ]; then
+sudo systemctl reload nginx
+fi
 }
 
 installmySQL() {
-  MYSQL_ROOT_PASSWORD=root
-  WP_DB_PASSWORD=root
-  WP_DB_USERNAME="root"
-  echo "mysql-server-5.7 mysql-server/root_password password $MYSQL_ROOT_PASSWORD" | sudo debconf-set-selections
-  echo "mysql-server-5.7 mysql-server/root_password_again password $MYSQL_ROOT_PASSWORD" | sudo debconf-set-selections
-  sudo apt install -y mysql-server
-  echo -e "MySql is installed for root user with password : $MYSQL_ROOT_PASSWORD"
+MYSQL_ROOT_PASSWORD=root
+WP_DB_PASSWORD=root
+WP_DB_USERNAME="root"
+echo "mysql-server-5.7 mysql-server/root_password password $MYSQL_ROOT_PASSWORD" | sudo debconf-set-selections
+echo "mysql-server-5.7 mysql-server/root_password_again password $MYSQL_ROOT_PASSWORD" | sudo debconf-set-selections
+sudo apt install -y mysql-server
+echo -e "MySql is installed for root user with password : $MYSQL_ROOT_PASSWORD"
 }
 
 mySQLConfig() {
-  mysql -u root -proot <<EOF
-    CREATE DATABASE $1 DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
-    GRANT ALL ON $1.* TO 'root'@'localhost' IDENTIFIED BY 'root';
-    FLUSH PRIVILEGES;
-    EXIT;
+mysql -u root -proot <<EOF
+CREATE DATABASE $1 DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+GRANT ALL ON $1.* TO 'root'@'localhost' IDENTIFIED BY 'root';
+FLUSH PRIVILEGES;
+EXIT;
 EOF
 }
 
 installPHP() {
-  sudo apt install -y php-fpm
-  sudo apt install -y php-curl php-gd php-intl php-mbstring php-soap php-xml php-xmlrpc php-zip
-  sudo systemctl restart php7.2-fpm
+sudo apt install -y php-fpm
+sudo apt install -y php-curl php-gd php-intl php-mbstring php-soap php-xml php-xmlrpc php-zip
+sudo systemctl restart php7.2-fpm
 }
 
 installWordPress() {
-  cd /tmp
-  curl -LO https://wordpress.org/latest.tar.gz
-  tar xzvf latest.tar.gz
-  cp /tmp/wordpress/wp-config-sample.php /tmp/wordpress/wp-config.php
+cd /tmp
+curl -LO https://wordpress.org/latest.tar.gz
+tar xzvf latest.tar.gz
+cp /tmp/wordpress/wp-config-sample.php /tmp/wordpress/wp-config.php
+sudo cp -a /tmp/wordpress/. /var/www/$1
 }
 
 configWordpress() {
@@ -106,4 +107,4 @@ else
   confConfig $1
 fi
 
-installWordPress
+installWordPress $1
