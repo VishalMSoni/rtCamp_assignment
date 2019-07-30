0a62a7a1f8cf1ac67e833f5f3787257e39dc9112
VishalMSoni <vishalsoni611@gmail.com>
2019-07-30 07:04:38 +0000

    Script modification

--- install.sh
+++ install.sh
@@ -86,9 +86,9 @@ function installmySQL() {
 }
 
 function mySQLConfig() {
-  mysql -u root -proot <<EOF
-    CREATE DATABASE wordpress DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
-    GRANT ALL ON wordpress.* TO 'root'@'localhost' IDENTIFIED BY 'root';
+  sudo mysql -u root -proot <<EOF
+    CREATE DATABASE example;
+    GRANT ALL ON wordpress.* TO 'root'@'localhost';
     FLUSH PRIVILEGES;
     EXIT;
 EOF
